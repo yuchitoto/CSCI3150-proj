4e176cd4c1f7fba669a89d913ceb460065a9b9b2
Yu Chi To <yuchitoto@gmail.com>
2019-12-18 16:38:37 +0800

    update

--- Project/Project-Prog/call.c
+++ Project/Project-Prog/call.c
@@ -1,10 +1,27 @@
 #include "call.h"
+#include <stdio.h>
+#include "superblock.h"
+#include "inode.h"
+
+#define KB 1024
+#define MB 1024*KB
+
+/*const superblock basics=
+{
+	.inode_offset = 4*KB;
+	.data_offset = 10*MB;
+	.max_inode = 100;
+	.max_data_blk = 25600;
+	.blk_size = 4*KB;
+};*/
+
 const char *HD = "HD";
 
 int open_t(char *pathname)
 {
 	int inode_number;
-	// write your code here.
+	FILE file = fopen("./HD","rb");
+	fseek(file,INODE_OFFSET,SEEK_SET);
 	return inode_number;
 }
 
@@ -12,7 +29,7 @@ int read_t(int inode_number, int offest, void *buf, int count)
 {
 	int read_bytes;
 	// write your code here.
-	return read_bytes; 
+	return read_bytes;
 }
 
 // you are allowed to create any auxiliary functions that can help your implementation. But only “open_t()” and "read_t()" are allowed to call these auxiliary functions.
